2ac70fa29e1bcc6174a260f680cbdfb8a3e2ddf6
Alexandrdrdrd <kands12349@gmail.com>
2021-08-07 10:28:01 +0300

    added testing

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
-class NumberFormatter():
-    def __init__(self, string) :
-        self.string=string
-    
-    def parseInt(self):
-        if self.string == "":
+class NumberFormatter:
+
+    def parseInt(self, string):
+        if string == "":
             return "Empty string is not applicable"
-        elif len(self.string) < 2 or len(self.string) > 32 ** 2 - 1:
+        elif len(string) < 2 or len(string) > 32 ** 2 - 1:
             return "Incorrect line length"
-        elif self.string.isdigit() or self.string[0] == "+" or self.string[0] == "-":
-            return int(self.string)
+        elif string.isdigit() or string[0] == "+" or string[0] == "-":
+            return int(string)
         else:
             return "Invalid input format"
 
 
-
-my_object = NumberFormatter("123")
-print(my_object.parseInt())
+my_object = NumberFormatter()
+print(my_object.parseInt(""))
--- tests/test_func.py
+++ tests/test_func.py
@@ -1,33 +1,33 @@
-from main import testing
+from main import NumberFormatter
 
-
+my_object = NumberFormatter()
 def test_empty_string():
-    assert testing("") == "Empty string is not applicable"
+    assert my_object.parseInt("") == "Empty string is not applicable"
 
 
 def test_only_digits():
-    assert testing("1234") == int(1234)
+    assert my_object.parseInt("1234") == int(1234)
 
 
 def test_plus_before_numeric_value():
-    assert testing("+1234") == 1234
+    assert my_object.parseInt("+1234") == 1234
 
 
 def test_minus_before_numeric_value():
-    assert testing("-1234") == -1234
+    assert my_object.parseInt("-1234") == -1234
 
 
 def test_the_string_is_shorter_than_two_characters():
-    assert testing("1") == "Incorrect line length"
+    assert my_object.parseInt("1") == "Incorrect line length"
 
 
 def test_symbol_before_digit():
-    assert testing("*1234") == "Invalid input format"
+    assert my_object.parseInt("*1234") == "Invalid input format"
 
 
 def test_character_after_digits():
-    assert testing("1234*") == "Invalid input format"
+    assert my_object.parseInt("1234*") == "Invalid input format"
 
 
 def test_character_between_digits():
-    assert testing("12*34") == "Invalid input format"
+    assert my_object.parseInt("12*34") == "Invalid input format"
